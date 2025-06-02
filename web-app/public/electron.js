410a4a3fb57503f823eca293c6fbdaab51b19915
jamiemetzger600 <65310670+jamiemetzger600@users.noreply.github.com>
2025-06-01 20:45:10 -0700

    Chore: Prevent DevTools from opening on launch

--- web-app/public/electron.js
+++ web-app/public/electron.js
@@ -27,7 +27,7 @@ function createWindow () {
   mainWindow.loadURL(startUrl);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
